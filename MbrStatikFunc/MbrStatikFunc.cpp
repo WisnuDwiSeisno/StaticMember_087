2df9940f17597c308a2961b7e4a39003d6920803
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-17 19:17:21 +0700

    mulai membuat int main

--- MbrStatikFunc/MbrStatikFunc.cpp
+++ MbrStatikFunc/MbrStatikFunc.cpp
@@ -2,4 +2,34 @@
 #include <string>
 using namespace std;
 
-class mahasiswa 
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+
+}
