e30c9176099740b8bbc1976538ae539a7616671d
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-17 17:39:47 +0700

    selesai membuat int main dan memanggil chain function

--- ChainFunction/ChainFunction.cpp
+++ ChainFunction/ChainFunction.cpp
@@ -18,5 +18,8 @@ public:
 } bukunya;
 
 int main() {
-
+	bukunya.setJudul("Matematika");
+	cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+	return 0;
 }
