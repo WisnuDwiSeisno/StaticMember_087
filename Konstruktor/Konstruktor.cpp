de3b83bf12df4aa610702b7b26969d4a24b77509
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-17 18:46:06 +0700

    mulai membuat int main

--- Konstruktor/Konstruktor.cpp
+++ Konstruktor/Konstruktor.cpp
@@ -31,3 +31,11 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+
+}
